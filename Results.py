6d4893c0aea78c2c4cc552e731bdf7cdc55ebfbf
emiliamzz <eziel034@uottawa.ca>
2021-02-07 16:40:17 -0500

    Step 5 fully works

--- Results.py
+++ Results.py
@@ -8,7 +8,7 @@ f = open("Results.txt", "w", encoding="utf-8")
 # Loop through each test query, get the important info
 # This includes the topic_id (between <num>) and the query (between <title>)
 for query in queries:
-    topicId = query.split("<num> Number: ")[1].split(" </num>")[0]
+    topicId = int(query.split("<num> Number: MB")[1].split(" </num>")[0])
     search = query.split("<title> ")[1].split(" </title")[0]
 # When each query runs through the retrieval, we should only be inputting the query
 # The output should be a dictionary (sorted by value) with the key as the tweet ID
@@ -20,6 +20,6 @@ for query in queries:
     count = 0
     for result in dictionary:
         count += 1
-        line = topicId + " Q0 " + result + " " + str(count) + " " + str(dictionary[result]) + " PyGER\n"
+        line = str(topicId) + " Q0 " + result + " " + str(count) + " " + str(dictionary[result]) + " PyGER\n"
         f.write(line)
 f.close()
