f93c77f3f603c769686d49027be2bade9ea3c562
emiliamzz <eziel034@uottawa.ca>
2021-03-14 22:11:25 -0400

    .

--- Day1.py
+++ Day1.py
@@ -6,6 +6,8 @@
 # Output:
 # - Modified Results.txt (let's name it Results-1.txt?)
 
+import operator
+import sys
 from sent2vec.vectorizer import Vectorizer
 from scipy import spatial
 
@@ -15,33 +17,45 @@ def main(resultss, tweetss, queriess):
     queries = open(queriess, encoding="utf-8").read().split("</top>\n\n<top>")
     results = open(resultss, encoding="utf-8").read().split("\n")
     t = open(tweetss, encoding="utf-8").read().split("\n")
+    f = open("Results-1.txt", "w", encoding="utf-8")
+    t[0] = t[0][1:len(t[0])]
     tweets = {}
     for tweet in t:
-        tw = split("/t")
-        tweets[tw[0]] = tw[1]
+        if tweet:
+            tw = tweet.split("\t")
+            tweets[tw[0]] = tw[1]
     for query in queries:
         topicId = int(query.split("<num> Number: MB")[1].split(" </num>")[0])
         search = query.split("<title> ")[1].split(" </title")[0]
         # grab results for that query
         relevantResults = {}
-        relevantTweets = []
-        relevantIds = []
+        # vectorize the query
+        vectorizer.bert([search])
+        queryVector = vectorizer.vectors[0]
+        queryNums = []
+        queryTweets = []
         for r in results:
             liner = r.split()
             if int(liner[0]) == topicId:
-                relevantResults[liner[2]] = None # None is going to be replaced by similarity score
-                relevantTweets.append(tweets[liner2])
-                relevantIds.append(liner[2])
-        # vectorize the query
-        queryVector = vectorizer.bert([search])
-        # vectorize the relevant tweets
-        tweetsVector = vectorizer.bert(relevantTweets)
-        # calculate the distance between each tweet and the query
-        for tweet in tweetsVector:
-            distance = spatial.distance.cosine(queryVector[0], tweet)
-            index = tweetsVector.index(tweet)
-            relevantResults[relevantIds[index]] = distance
-    # return
+                queryTweets.append(tweets[liner[2]])
+                queryNums.append(liner[2])
+            elif int(liner[0]) > topicId:
+                break
+        vectorizer.bert(queryTweets)
+        for i in range(len(queryNums)):
+            tweetVector = vectorizer.vectors
+            distance = spatial.distance.cosine(queryVector, tweetVector[i])
+            relevantResults[queryNums[i]] = distance
+        relevantResults = dict(sorted(relevantResults.items(), key=operator.itemgetter(1), reverse=True))
+        # print to results
+        count = 0
+        for result in relevantResults:
+            count += 1
+            line = str(topicId) + " Q0 " + result + " " + str(count) + " " + str(relevantResults[result]) + " PyGER\n"
+            f.write(line)
+    f.close()
+    
 
 if __name__ == "__main__":
-    main(str(sys.argv[1]), str(sys.argv[2]), str(sys.argv[3]))
+    main("./Results.txt", "./Trec_microblog11.txt", "./topics_MB1-49.txt")
+    #main(str(sys.argv[1]), str(sys.argv[2]), str(sys.argv[3]))
